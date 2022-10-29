26ebf963e325535ad14c9c0c4bd0c7645e908ef9
CoolsomeGamer21 <105989804+CoolsomeGamer21@users.noreply.github.com>
2022-10-29 17:23:29 -0400

    Create spygame.js

---
+++ frontend/spygame.js
@@ -0,0 +1,185 @@
+<html>
+ /* Rectangle 1 */
+
+
+position: absolute;
+width: 1512px;
+height: 982px;
+left: 0px;
+top: 0px;
+
+background: #1DDED2;
+
+
+/* Rectangle 12 */
+
+
+position: absolute;
+width: 1512px;
+height: 62px;
+left: 0px;
+top: 0px;
+
+background: #000000;
+
+
+/* spyvpn.tech/vpn/success */
+
+
+position: absolute;
+width: 1512px;
+height: 62px;
+left: 0px;
+top: 10px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 40px;
+line-height: 48px;
+
+color: #FFFFFF;
+
+
+
+/* WELCOME TO ESCAPE ROOM- BREAKOUT EDITION */
+
+
+position: absolute;
+width: 1352px;
+height: 150px;
+left: 90px;
+top: 86px;
+
+font-family: 'News Cycle';
+font-style: normal;
+font-weight: 400;
+font-size: 64px;
+line-height: 105px;
+
+color: #E72626;
+
+
+
+/* Encrypted by SpyVPN */
+
+
+position: absolute;
+width: 220px;
+height: 68px;
+left: 14px;
+top: 72px;
+
+font-family: 'News Cycle';
+font-style: normal;
+font-weight: 400;
+font-size: 24px;
+line-height: 39px;
+
+color: #000000;
+
+
+
+/* Rectangle 26 */
+
+
+position: absolute;
+width: 230px;
+height: 80px;
+left: 90px;
+top: 616px;
+
+background: #E72626;
+
+
+/* Rectangle 27 */
+
+
+position: absolute;
+width: 230px;
+height: 80px;
+left: 674px;
+top: 616px;
+
+background: #D7E726;
+
+
+/* Rectangle 28 */
+
+
+position: absolute;
+width: 230px;
+height: 80px;
+left: 1209px;
+top: 616px;
+
+background: #00FF19;
+
+
+/* image 7 */
+
+
+position: absolute;
+width: 842px;
+height: 355px;
+left: 320px;
+top: 189px;
+
+background: url(image.png);
+
+
+/* EASY */
+
+
+position: absolute;
+width: 406px;
+height: 60px;
+left: 124px;
+top: 616px;
+
+font-family: 'Noticia Text';
+font-style: normal;
+font-weight: 700;
+font-size: 64px;
+line-height: 87px;
+
+color: #000000;
+
+
+
+/* HARD */
+
+
+position: absolute;
+width: 406px;
+height: 60px;
+left: 701px;
+top: 616px;
+
+font-family: 'Noticia Text';
+font-style: normal;
+font-weight: 700;
+font-size: 64px;
+line-height: 87px;
+
+color: #000000;
+
+
+
+/* CHALLENGE */
+
+
+position: absolute;
+width: 406px;
+height: 60px;
+left: 1209px;
+top: 633px;
+
+font-family: 'Noticia Text';
+font-style: normal;
+font-weight: 700;
+font-size: 40px;
+line-height: 55px;
+
+color: #000000;
+</html>
